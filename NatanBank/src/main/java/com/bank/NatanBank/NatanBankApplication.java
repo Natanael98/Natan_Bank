cf0a5d9e29c604e8a2910e902f3a175589a83209
Natanael Henrique <nhcardoso4@gmail.com>
2020-03-13 15:53:58 -0300

    Criação da Arquiterua Inicial
    Tecnolgias utilizadas :
    Spring Boot/MVC/Data
    JPA
    Hibernate
    H2
    MySQL

---
+++ NatanBank/src/main/java/com/bank/NatanBank/NatanBankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.NatanBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatanBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatanBankApplication.class, args);
+	}
+
+}
---
+++ NatanBank/src/test/java/com/bank/NatanBank/NatanBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.NatanBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatanBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
